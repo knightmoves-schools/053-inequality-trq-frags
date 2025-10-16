5afa593211bd219481c63f39c7f7e133689684d4
track-frags <jadirks@anamosa.k12.ia.us>
2025-10-16 18:00:11 +0000

    sdiybt

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function guessNumber(guess) {
+    if (guess != 25) {
+        return "not it";
+    }
+}
 
+function strictGuessNumber(guess) {
+    if (typeof guess === "number" && guess !== 25) {
+        return "not it";
+    }
+}
+
+module.exports = { guessNumber, strictGuessNumber };
